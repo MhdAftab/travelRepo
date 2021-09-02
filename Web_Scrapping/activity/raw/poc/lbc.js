cabc20d1ad6edf6dc9310cef3d6a98eded5302f2
MhdAftab <soundcore42000@gmail.com>
2021-09-02 15:04:27 +0530

    Last Ball Commentary: Cheerio

--- Web_Scrapping/activity/raw/poc/lbc.js
+++ Web_Scrapping/activity/raw/poc/lbc.js
@@ -2,7 +2,8 @@
 let request = require("request");
 let cheerio = require("cheerio"); // for data extraction
 console.log("Before");
-request("https://www.npmjs.com/package/cheerio", cb);
+let url = "https://www.espncricinfo.com/series/ipl-2021-1249214/delhi-capitals-vs-royal-challengers-bangalore-22nd-match-1254079/ball-by-ball-commentary";
+request(url, cb);
 function cb(error, response, html){ // it is s async function.. execute after the main line of file get executed
     if(error){
         console.log(error);
@@ -19,9 +20,12 @@ function dataExtractor(html){
     // search tool
     let searchTool = cheerio.load(html);
     // css selector 
-    let elemRep = searchTool("#readme>h1");
-    let moduleName = elemRep.text().trim();
-    console.log("Module Name : ", moduleName);
+    let elemRep = searchTool(".match-comment .match-comment-long-text.match-comment-padder>p");
+    
+    let lbc = searchTool(elemRep[3]).text();
+    console.log(lbc);
 }
 
 console.log("After");
+
+// .match-comment .match-comment-long-text.match-comment-padder
